92889727f7b9ea708a112f79375fa44c57381e37
ksolo-2022439 <ksolorzano-2022439@kinal.edu.gt>
2026-02-10 11:38:21 -0600

    feat: Añadir archivos escenciales

---
+++ src/AuthService.Domain/Constants/RoleConstants.cs
@@ -0,0 +1,9 @@
+namespace AuthService.Domain.Constants;
+ 
+public class Constants
+{
+    public const string ADMIN_ROLE = "ADMIN_ROLE";
+    public const string USER_ROLE = "USER_ROLE";
+    public static readonly string[] AllowedRoles = { ADMIN_ROLE, USER_ROLE};
+   
+}
---
+++ src/AuthService.Domain/Enums/UserRole.cs
@@ -0,0 +1,11 @@
+// Espacio de nombres
+namespace AuthService.Domain.Enums;
+ 
+//Enumeración de roles de usuario
+//Cada valor tienen un valor numerico asociuado
+public enum UserRole
+{
+    User = 0,
+    Admin = 1,
+    Moderator = 2
+}
